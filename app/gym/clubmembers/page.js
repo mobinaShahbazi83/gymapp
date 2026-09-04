26ab20dbf677b216c6c9381cc1814d96fd785d76
mobinaShahbazi83 <shzmobina249@gmail.com>
2026-09-04 04:54:06 -0700

    Development of the Pena Jim user page

---
+++ app/gym/clubmembers/page.js
@@ -0,0 +1,8 @@
+import ClubmembersPage from "../../../pages/ClubmembersPage"
+
+
+function page() {
+  return (<ClubmembersPage/>)
+}
+
+export default page
---
+++ pages/ClubmembersPage.jsx
@@ -0,0 +1,111 @@
+import { FaRegSquare } from "react-icons/fa";
+import { CiMenuKebab } from "react-icons/ci";
+
+function ClubmembersPage() {
+    return (
+        <div className="w-full h-[1110px]">
+            <h1 className="!font-bold text-2xl flex justify-end items-center gap-8 mt-8 mr-10">لیست کاربران</h1>
+            <div className="w-[1600px] h-[750px]  mx-auto mt-10 rounded-4xl">
+               <div className="flex flex-col">
+                 <div className="flex justify-center pt-10 pb-3 pr-15 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-lg">تراکنش‌ها</p>
+                    <p className="!font-semibold text-lg">سانس</p>
+                    <p className="!font-semibold text-lg">کد ملی</p>
+                    <p className="!font-semibold text-lg">شماره تماس</p>
+                    <p className="!font-semibold text-lg">وضعیت</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">کاربر</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2"/>
+                    </div>
+                </div>
+                 <div className="flex justify-center pt-10 pb-3 pr-5 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-sm">مشاهده</p>
+                    <p className="!font-semibold text-sm">آزمایشی</p>
+                    <p className="!font-semibold text-sm">۵۰۸۰۱۷۹۸۵۶</p>
+                    <p className="!font-semibold text-sm">۰۹۲۱۷۷۸۳۵۲۴</p>
+                    <p className="!font-semibold text-sm">فعال</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">مریم فتحی</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2 "/>
+                        <CiMenuKebab className="text-[#289DFC] mt-2 w-[15px] h-[15px] " />
+                    </div>
+                </div>
+                  <div className="flex justify-center pt-10 pb-3 pr-5 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-sm">مشاهده</p>
+                    <p className="!font-semibold text-sm">آزمایشی</p>
+                    <p className="!font-semibold text-sm">۵۰۸۰۱۷۹۸۵۶</p>
+                    <p className="!font-semibold text-sm">۰۹۲۱۷۷۸۳۵۲۴</p>
+                    <p className="!font-semibold text-sm">فعال</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">مریم فتحی</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2 "/>
+                        <CiMenuKebab className="text-[#289DFC] mt-2 w-[15px] h-[15px] " />
+                    </div>
+                </div>
+                  <div className="flex justify-center pt-10 pb-3 pr-5 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-sm">مشاهده</p>
+                    <p className="!font-semibold text-sm">آزمایشی</p>
+                    <p className="!font-semibold text-sm">۵۰۸۰۱۷۹۸۵۶</p>
+                    <p className="!font-semibold text-sm">۰۹۲۱۷۷۸۳۵۲۴</p>
+                    <p className="!font-semibold text-sm">فعال</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">مریم فتحی</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2 "/>
+                        <CiMenuKebab className="text-[#289DFC] mt-2 w-[15px] h-[15px] " />
+                    </div>
+                </div>
+                   <div className="flex justify-center pt-10 pb-3 pr-5 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-sm">مشاهده</p>
+                    <p className="!font-semibold text-sm">آزمایشی</p>
+                    <p className="!font-semibold text-sm">۵۰۸۰۱۷۹۸۵۶</p>
+                    <p className="!font-semibold text-sm">۰۹۲۱۷۷۸۳۵۲۴</p>
+                    <p className="!font-semibold text-sm">فعال</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">مریم فتحی</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2 "/>
+                        <CiMenuKebab className="text-[#289DFC] mt-2 w-[15px] h-[15px] " />
+                    </div>
+                </div>
+                   <div className="flex justify-center pt-10 pb-3 pr-5 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-sm">مشاهده</p>
+                    <p className="!font-semibold text-sm">آزمایشی</p>
+                    <p className="!font-semibold text-sm">۵۰۸۰۱۷۹۸۵۶</p>
+                    <p className="!font-semibold text-sm">۰۹۲۱۷۷۸۳۵۲۴</p>
+                    <p className="!font-semibold text-sm">فعال</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">مریم فتحی</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2 "/>
+                        <CiMenuKebab className="text-[#289DFC] mt-2 w-[15px] h-[15px] " />
+                    </div>
+                </div>
+                  <div className="flex justify-center pt-10 pb-3 pr-5 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-sm">مشاهده</p>
+                    <p className="!font-semibold text-sm">آزمایشی</p>
+                    <p className="!font-semibold text-sm">۵۰۸۰۱۷۹۸۵۶</p>
+                    <p className="!font-semibold text-sm">۰۹۲۱۷۷۸۳۵۲۴</p>
+                    <p className="!font-semibold text-sm">فعال</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">مریم فتحی</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2 "/>
+                        <CiMenuKebab className="text-[#289DFC] mt-2 w-[15px] h-[15px] " />
+                    </div>
+                </div>
+                  <div className="flex justify-center pt-10 pb-3 pr-5 gap-50  border-b border-[#EFEFEF]">
+                    <p className="!font-semibold text-sm">مشاهده</p>
+                    <p className="!font-semibold text-sm">آزمایشی</p>
+                    <p className="!font-semibold text-sm">۵۰۸۰۱۷۹۸۵۶</p>
+                    <p className="!font-semibold text-sm">۰۹۲۱۷۷۸۳۵۲۴</p>
+                    <p className="!font-semibold text-sm">فعال</p>
+                    <div className="flex gap-2">                       
+                        <p className="!font-semibold text-lg">مریم فتحی</p>
+                        <FaRegSquare  className="text-[#289DFC] mt-2 "/>
+                        <CiMenuKebab className="text-[#289DFC] mt-2 w-[15px] h-[15px] " />
+                    </div>
+                </div>
+               </div>
+            </div>
+        </div>
+    )
+}
+
+export default ClubmembersPage
